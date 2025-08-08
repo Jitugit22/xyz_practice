35c36dcd8da0ebfe85314dfdfbfce2af96c170c2
Jitu <jitenderyadav86@gmail.com>
2025-08-08 22:43:31 +0530

    add

--- child/storage/main.tf
+++ child/storage/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "ATOZ01Storage" {
-  name                     = "shivastgaccount"
+  name                     = "shivastg"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
